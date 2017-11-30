4c4aafc9aa3836be4131446952b8bc9779dceede
Frank Knark <frank@example.com>
2017-11-30 12:25:35 -0500

    fixed injection of datasource

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -1,95 +1,88 @@
 package io.pivotal.pal.tracker;
 
-import com.mysql.cj.jdbc.PreparedStatement;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.Date;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.sql.PreparedStatement;
 import java.util.List;
-import java.util.Map;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class JdbcTimeEntryRepository implements TimeEntryRepository {
 
-    private JdbcTemplate jdbcTemplate;
-private DataSource _dataSource;
+    private final JdbcTemplate jdbcTemplate;
+
     public JdbcTimeEntryRepository(DataSource dataSource) {
-this._dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
     @Override
     public TimeEntry create(TimeEntry timeEntry) {
-        String insertQuery = "INSERT INTO time_entries (project_id,user_id,date,hours) VALUES (?,?,?,?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        this.jdbcTemplate.update(connection -> {
-            PreparedStatement ps = (PreparedStatement) connection.prepareStatement(insertQuery, new String[] {"id"});
-            ps.setString(1, String.valueOf(timeEntry.getProjectId()));
-            ps.setString(2, String.valueOf(timeEntry.getUserId()));
-            ps.setString(3, String.valueOf(timeEntry.getDate()));
-            ps.setString(4, String.valueOf(timeEntry.getHours()));
-            return ps;
-        }, keyHolder);
-        long id = keyHolder.getKey().longValue(); //now contains the generated key}
-        return find(id);
+        KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO time_entries (project_id, user_id, date, hours) " +
+                            "VALUES (?, ?, ?, ?)",
+                    RETURN_GENERATED_KEYS
+            );
+
+            statement.setLong(1, timeEntry.getProjectId());
+            statement.setLong(2, timeEntry.getUserId());
+            statement.setDate(3, Date.valueOf(timeEntry.getDate()));
+            statement.setInt(4, (int)timeEntry.getHours());
+
+            return statement;
+        }, generatedKeyHolder);
+
+        return find(generatedKeyHolder.getKey().longValue());
     }
 
     @Override
     public TimeEntry find(Long id) {
-        try {
-            Map<String, Object> foundEntry = jdbcTemplate.queryForMap("Select * from time_entries where id = ?", id);
-
-            return getTimeEntryFromDbRecord(foundEntry);
-        }
-        catch(Exception e) {
-            return null;
-        }
-
-    }
-
-    private TimeEntry getTimeEntryFromDbRecord(Map<String, Object> foundEntry) {
-        long idFromDb = (long)foundEntry.get("id");
-        long projectIdFromDb = ((long) foundEntry.get("project_id"));
-        long userIdFromDb = (long)foundEntry.get("user_id");
-        Date dateFromDb = (Date)foundEntry.get("date");
-        LocalDate localDate = dateFromDb.toLocalDate();
-        int hoursFromDb = (int)foundEntry.get("hours");
-        return new TimeEntry(idFromDb,projectIdFromDb,userIdFromDb,localDate,hoursFromDb);
+        return jdbcTemplate.query(
+                "SELECT id, project_id, user_id, date, hours FROM time_entries WHERE id = ?",
+                new Object[]{id},
+                extractor);
     }
 
     @Override
     public List<TimeEntry> list() {
-        List<Map<String,Object>> foundEntries = jdbcTemplate.queryForList("select * from time_entries");
-        List<TimeEntry> mappedTimeEntries = new ArrayList<>();
-        for (Map<String,Object> item: foundEntries) {
-            mappedTimeEntries.add(getTimeEntryFromDbRecord(item));
-        }
-        return mappedTimeEntries;
+        return jdbcTemplate.query("SELECT id, project_id, user_id, date, hours FROM time_entries", mapper);
     }
 
     @Override
     public TimeEntry update(Long id, TimeEntry timeEntry) {
-        String insertQuery = "UPDATE time_entries SET project_id = ?, user_id = ?, date = ?, hours = ? WHERE id = ?";
-        this.jdbcTemplate.update(connection -> {
-            PreparedStatement ps = (PreparedStatement) connection.prepareStatement(insertQuery);
-            ps.setString(1, String.valueOf(timeEntry.getProjectId()));
-            ps.setString(2, String.valueOf(timeEntry.getUserId()));
-            ps.setString(3, String.valueOf(timeEntry.getDate()));
-            ps.setString(4, String.valueOf(timeEntry.getHours()));
-            ps.setString(5, String.valueOf(id));
-            return ps;
-        });
+        jdbcTemplate.update("UPDATE time_entries " +
+                        "SET project_id = ?, user_id = ?, date = ?,  hours = ? " +
+                        "WHERE id = ?",
+                timeEntry.getProjectId(),
+                timeEntry.getUserId(),
+                Date.valueOf(timeEntry.getDate()),
+                timeEntry.getHours(),
+                id);
+
         return find(id);
     }
 
     @Override
     public void delete(Long id) {
-        this.jdbcTemplate.execute("DELETE from time_entries WHERE id = "+id);
+        jdbcTemplate.update("DELETE FROM time_entries WHERE id = ?", id);
     }
+
+    private final RowMapper<TimeEntry> mapper = (rs, rowNum) -> new TimeEntry(
+            rs.getLong("id"),
+            rs.getLong("project_id"),
+            rs.getLong("user_id"),
+            rs.getDate("date").toLocalDate(),
+            rs.getInt("hours")
+    );
+
+    private final ResultSetExtractor<TimeEntry> extractor =
+            (rs) -> rs.next() ? mapper.mapRow(rs, 1) : null;
 }
--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,33 +1,31 @@
 package io.pivotal.pal.tracker;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import com.mysql.cj.jdbc.MysqlDataSource;
+        import com.fasterxml.jackson.annotation.JsonInclude;
+        import com.fasterxml.jackson.databind.ObjectMapper;
+        import com.fasterxml.jackson.databind.SerializationFeature;
+        import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-import javax.sql.DataSource;
+        import javax.sql.DataSource;
+        import java.util.TimeZone;
 
 @SpringBootApplication
 public class PalTrackerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(PalTrackerApplication.class,args);
-
+        // Make sure the application runs as UTC
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    private DataSource getDataSource() {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
-        return dataSource;
-    }
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new JdbcTimeEntryRepository(getDataSource());
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
+
     @Bean
     public ObjectMapper jsonObjectMapper() {
         return Jackson2ObjectMapperBuilder.json()
